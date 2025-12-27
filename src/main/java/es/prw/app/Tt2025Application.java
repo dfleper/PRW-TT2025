6d1813338b9915ef22a6a54284ba98127785e577
dfleper <124105822+dfleper@users.noreply.github.com>
2025-12-27 01:58:55 +0000

    feat: Add TT2025 codebase.

---
+++ src/main/java/es/prw/app/Tt2025Application.java
@@ -0,0 +1,13 @@
+package es.prw.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "es.prw")
+public class Tt2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tt2025Application.class, args);
+	}
+
+}
